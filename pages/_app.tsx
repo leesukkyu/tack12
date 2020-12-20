b77f78b7dd69b45eaf454614171d1dd9083aebc4
seokkyu.lee <seokkyu.lee@navercorp.com>
2020-12-20 19:41:41 +0900

    아이콘 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../styles/globals.scss'
+import 'material-design-icons/iconfont/material-icons.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    return <Component {...pageProps} />
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,37 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.scss'
-import { applySession } from 'next-session';
-import sessionConfig from '../config/session'
+import {applySession} from 'next-session'
+import {sessionConfig, helmetConfig} from '../config'
 
-function Home({views, name}){
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+function Home({views, name}) {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>{helmetConfig.title}</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main className={styles.main}>
-        로그인 전 {views} {name} {process.env.NEXT_PUBLIC_ANALYTICS_ID}
-      </main>
-{process.env.NEXT_PUBLIC_ANALYTICS_ID}
-      <footer className={styles.footer}>
-        footer
-      </footer>
-    </div>
-  )
+            <main className={styles.main}>
+                <i className="material-icons">face</i>
+                로그인 전 {views} {name} {process.env.NEXT_PUBLIC_ANALYTICS_ID}
+            </main>
+
+            <footer className={styles.footer}>footer</footer>
+        </div>
+    )
 }
 
-export async function getServerSideProps({ req, res }) {
-  await applySession(req, res, sessionConfig);
-  req.session.views = req.session.views ? req.session.views + 1 : 1;
-  req.session.name = '이석규'
-  return {
-    props: {
-      views: req.session.views,
-      name: req.session.name
+export async function getServerSideProps({req, res}) {
+    await applySession(req, res, sessionConfig)
+    req.session.views = req.session.views ? req.session.views + 1 : 1
+    req.session.name = '이석규'
+    return {
+        props: {
+            views: req.session.views,
+            name: req.session.name,
+        },
     }
-  }
 }
 
 export default Home
